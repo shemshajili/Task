2d7ee4e5167ccea56664f0c5cfac3bf0e9fbc44c
shemshajili <shamsah@code.edu.az>
2023-03-02 12:40:52 +0400

    repeat task

---
+++ Repeat task/Repeat task/Program.cs
@@ -0,0 +1,12 @@
+﻿string repeat(string word, int count)
+{
+    string line = "";
+
+    for (int i = 1; i <= count; i++)
+    {
+        line += word;
+    }
+    Console.WriteLine(line);
+    return line;
+}
+repeat("Salam Aleykum", 3);
